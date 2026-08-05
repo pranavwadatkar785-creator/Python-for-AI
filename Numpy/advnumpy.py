984d759036d6dbe0a0382138462636c6a5e75849
pranavwadatkar785-creator <pranavwadatkar785@gmail.com>
2026-08-05 10:32:31 +0530

    Numpy

---
+++ Numpy/advnumpy.py
@@ -0,0 +1,104 @@
+import numpy as np
+import numpy.ma as ma
+
+# broadcasting
+a = np.array([1,2,3])
+b = np.array([[4],[5]])
+
+print(a + b)
+
+# dimensional indexing
+a = np.array([[[1,2,3],
+               [4,5,6],
+               [7,8,9]],
+              [[9,8,7],
+               [6,5,4],
+               [3,2,1]]])
+print(np.shape(a))
+print('=============================')
+#       F,R,C
+print(a[0,2,2])
+print(a[:,1,1])
+print('=============================')
+# newaxis
+print(a[:,1,1,np.newaxis])
+print('=============================')
+#        Face
+print(a[[0,1]])
+print('=============================')
+#        F    R
+print(a[[1],[0,2]])
+print('=============================')
+#        F    Rows  Columns
+print(a[[0],[0,1,2],[0,1,2]])
+print('=============================')
+#       Face       Row               Column
+print(a[[0],[True,False,False],[False,True,True]])
+
+
+# sorting and searching
+a = np.array([[5,4,9],
+              [1,7,3],
+              [6,2,8]])
+
+print(np.sort(np.sort(a,axis=0),axis=1))
+
+print(np.sort(a.flatten()).reshape(a.shape))
+print(a.flatten())
+print(a.argmax()) #index of highest value
+
+# iteration
+a = np.array([[5,4,9],
+              [1,7,3],
+              [6,2,8]])
+for x in np.nditer(a, order="F"): # C- for normal row wise, F- for column wise
+    print(x, end=" ")
+print()
+
+with np.nditer(a, op_flags=["readwrite"]) as it:
+    for element in it:
+        element[...] = element**2
+
+print(a)
+
+# masking array import numpy.ma as ma
+arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
+masked_arr = ma.masked_array(arr, mask=[[0,1,0],[0,0,1],[1,0,0]])
+print(masked_arr)
+print(masked_arr.sum())
+print(ma.getmask(masked_arr))
+
+arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(ma.masked_greater(arr, value=5))
+print(ma.masked_less(arr, value=5))
+
+print(ma.masked_inside(arr, v1=3, v2=7))
+print(ma.masked_outside(arr, v1=3, v2=7))
+print(ma.masked_where(arr % 2 !=0, arr))
+
+#vectorization and matrix multiplication
+arr = np.array([1,2,3,4,5])
+
+def square(x):
+    if x % 2 == 0:
+        return x**2
+    else:
+        return x
+
+vectorized_square = np.vectorize(square)
+print(vectorized_square(arr))
+
+A = np.array([[1,2],
+              [4,5]])
+B = np.array([[7,8],
+              [10,11]])
+print(np.shape(A))
+print(np.shape(B))
+print(np.matmul(A,B)) # or print(A @ B)
+
+
+# custom data types
+dt = np.dtype([('name', 'S10'), ('age', 'i4'), ('height', 'f4')])
+data = np.array([('Alice', 25, 5.5), ('Bob', 30, 6.0), ('Charlie', 35, 5.8)], dtype=dt)
+print(data.dtype)
+
---
+++ Numpy/numpyy.py
@@ -0,0 +1,97 @@
+import numpy as np 
+list1= [1,2,3,4] 
+arr1 = np.array(list1) 
+print(type(arr1)) 
+arr2d = np.array([list1,list1]) 
+print(arr2d,arr2d.ndim) 
+arr2d = np.array([[list1,list1,list1]]) 
+print(arr2d,arr2d.ndim) 
+#basic functionc in numpy 
+arr1 = np.array([[[1,2,3,4],[5,6,7,8],[1, 2, 3, 4]],[[1,2,3,4],[5,6,7,8],[1, 2, 3, 4]]]) 
+print(arr1) 
+print("n_dim",arr1.ndim) 
+print("shape",arr1.shape) 
+print("size",arr1.size) 
+print("dtype",arr1.dtype) 
+# array initialization 
+# zero array 
+zer0_arr = np.zeros((1,2,3)) 
+print("Zero array\n",zer0_arr) 
+# one array 
+one_arr = np.ones((3,2)) 
+print("one array\n",one_arr) 
+# Full array 
+full_arr = np.full((1,2,3),15) 
+print("Full array\n",full_arr) 
+# identity matrix 
+id_arr = np.eye(3) 
+print("Identity matrix\n",id_arr) 
+print(np.empty((4,2))) 
+print(np.arange(0,21,3)) 
+print(np.linspace(0,20,6)) 
+print(np.random.rand(2,3)) 
+print(np.random.randint(1,100,(3,3))) 
+# array indexing and slicing 
+a = np.arange(1,10,2) 
+print(a) 
+#slicing 
+print(a[0:3:1]) 
+two_D = np.array([[1,2,3],[4,5,6]]) 
+print(two_D[0][1]) 
+print(two_D[1]) 
+print(two_D[1:]) 
+#reshapeing and flattening 
+list1= [1,2,3,4] 
+list2= [5,6,7,8] 
+arr1 = np.array([list1,list2]) 
+arr1.reshape(4,2) 
+arr1.reshape(8) 
+arr1.reshape([2,2,2]) 
+arr1.flatten() 
+#vertical and horizontal stacking 
+a = np.array([1,2,3,4]) 
+b = np.array([5,6,7,8]) 
+print(np.vstack((a,b))) 
+print(np.hstack((a,b))) 
+# Basic arithmetic operations 
+print(np.add(a,b)) 
+print(np.subtract(a,b)) 
+print(np.multiply(a,b)) 
+print(np.divide(a,b)) 
+#dot product 
+print(np.dot(a,b)) 
+#Transpose 
+arr1 = np.array([[1,2,3],[4,5,6]]) 
+print(arr1.T) 
+#statistics 
+arr1 = np.array([[1,2,3], [4,5,6]]) 
+print("sum",np.sum(arr1)) 
+print("sum along axis 0",np.sum(arr1,axis=0)) 
+print("sum along axis 1",np.sum(arr1,axis=1)) 
+print("mean",np.mean(arr1)) 
+print("median",np.median(arr1)) 
+print("std",np.std(arr1)) 
+print("var",np.var(arr1)) 
+print("min",np.min(arr1)) 
+print("max",np.max(arr1)) 
+print("avg",np.average(arr1)) 
+#array comparison 
+a = np.array([1,2,3,4]) 
+b = np.array([5,6,7,8]) 
+print(a == b) 
+print(np.array_equal(a,b)) 
+print(a < b) 
+#Broadcasting 
+a = np.array([[1,2,3],[4,5,6]]) 
+b = np.array([1,2,3]) 
+print(a + b) 
+print(a * b) 
+#Handling nan 
+data = np.array([[1,2,3],[4,np.nan,6],[7,8,9]]) 
+print(np.nan_to_num(data)) 
+#save and load 
+a = np.array([[1,2,3],[4,5,6]]) 
+np.save('array.npy', a) 
+a = np.arange(1,26).reshape(5,5) 
+print("Last row",a[4]) 
+print("1st column",a[:,0])
